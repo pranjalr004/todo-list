093f85878deb67ca045a226a19457fb2772e9e41
pranjalr004 <pranjalr376@gmail.com>
2025-02-12 20:11:49 +0530

    Delete and Add buttons are created

--- todolist/app/page.js
+++ todolist/app/page.js
@@ -1,16 +1,66 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 
 const page = () => {
+  const [title, setTitle] = useState("")
+  const [desc, setdesc] = useState("")
+  const [mainTask, setMainTask] = useState([])
+
+  const submitHandler=(e)=>{
+    e.preventDefault()
+    setMainTask([...mainTask,{title,desc}]) 
+    setTitle("")
+    setdesc("")
+    console.log(mainTask)
+  }
+
+  const deleteHandler=(i)=>{
+    let copyTask=[...mainTask]
+    copyTask.splice(i,1)
+    setMainTask(copyTask)
+  }
+
+  let renderTask=<h2>No Task Available</h2>
+
+  if(mainTask.length>0){
+  renderTask=mainTask.map((t,i)=>{
+    return (
+      <li key={i} className='flex items-center justify-between mb-8'>
+      <div className='flex items-center justify-between mb-5 w-2/3'>
+        <h5 className='text-2xl font-semibold'>{t.title}</h5>
+        <h6 className='text-lg font-medium'>{t.desc}</h6>
+      </div>
+      <button className='bg-red-400 text-white px-4 py-2 rounded font-bold' onClick={()=>{
+        deleteHandler(i)
+      }}>Delete</button>
+      </li>
+    )
+  })
+}
+
   return (
-   <>
+    <>
     <h1 className='bg-black text-white p-5 text-3xl font-bold text-center'>My Todo List</h1>
 
-    <form>
-      <input type='text' className='text-2xl border-zinc-800 border-4 m-8 px-4 py-2' placeholder='Enter the task'/>
-      <input type='text' className='text-2xl border-zinc-800 border-4 m-8 px-4 py-2' placeholder='Enter Description here'/>
+    <form onSubmit={submitHandler}>
+      <input type='text' className='text-2xl border-zinc-800 border-4 m-8 px-4 py-2' placeholder='Enter the task' value={title} onChange={(e)=>{
+        setTitle(e.target.value)
+      }}/>
+
+
+      <input type='text' className='text-2xl border-zinc-800 border-4 m-8 px-4 py-2' placeholder='Enter Description here' value={desc} onChange={(e)=>{
+        setdesc(e.target.value)
+      }}/>
 
       <button className='bg-black text-white px-4 py-3 m-5 text-2xl font-bold rounded'>Add Task</button>
+    
     </form>
+    <hr/>
+    <div className='bg-slate-200 p-8'>
+      <ul>
+        {renderTask}
+      </ul>
+    </div>
    </>
   )
 }
